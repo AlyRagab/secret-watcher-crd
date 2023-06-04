1c4e89cb52b39734048aa61751f7b1ac47158605
Aly <alyragab70@gmail.com>
2023-06-04 04:43:59 +0300

    implement the API

--- api/v1/secretwatcher_types.go
+++ api/v1/secretwatcher_types.go
@@ -1,45 +1,17 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // SecretWatcherSpec defines the desired state of SecretWatcher
 type SecretWatcherSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of SecretWatcher. Edit secretwatcher_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // SecretWatcherStatus defines the observed state of SecretWatcher
 type SecretWatcherStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // SecretWatcher is the Schema for the secretwatchers API
 type SecretWatcher struct {
@@ -50,7 +22,7 @@ type SecretWatcher struct {
 	Status SecretWatcherStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
 // SecretWatcherList contains a list of SecretWatcher
 type SecretWatcherList struct {
--- controllers/secretwatcher_controller.go
+++ controllers/secretwatcher_controller.go
@@ -1,62 +1,56 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
+	"fmt"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	v1 "github.com/AlyRagab/secret-watcher-crd/api/v1"
+	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	secretwatcherv1 "github.com/AlyRagab/secret-watcher-crd/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // SecretWatcherReconciler reconciles a SecretWatcher object
 type SecretWatcherReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=secretwatcher.secretwatcher.aly.com,resources=secretwatchers,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=secretwatcher.secretwatcher.aly.com,resources=secretwatchers/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=secretwatcher.secretwatcher.aly.com,resources=secretwatchers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=aly.com,resources=secretwatchers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SecretWatcher object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *SecretWatcherReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// Fetch the SecretWatcher instance
+	secretWatcher := &v1.SecretWatcher{}
+	err := r.Get(ctx, req.NamespacedName, secretWatcher)
+	if err != nil {
+		// Error reading the object - requeue the request
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// TODO(user): your logic here
+	// List all Secrets in all namespaces
+	secrets := &corev1.SecretList{}
+	err = r.List(ctx, secrets, client.InNamespace(corev1.NamespaceAll))
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Handle each Secret
+	for _, secret := range secrets.Items {
+		// Process the Secret
+		fmt.Printf("Processing Secret: %s/%s\n", secret.Namespace, secret.Name)
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *SecretWatcherReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func SetupWithManager(mgr manager.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&secretwatcherv1.SecretWatcher{}).
-		Complete(r)
+		For(&v1.SecretWatcher{}).
+		Watches(&source.Kind{Type: &corev1.Secret{}}, &handler.EnqueueRequestForObject{}).
+		Complete(&SecretWatcherReconciler{
+			Client: mgr.GetClient(),
+		})
 }
